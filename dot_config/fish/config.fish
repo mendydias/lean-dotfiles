e59022f242c2df192bf2e5adf2575633a3a867a7
Umendya Ranmal Dias <dev.mendy.das@gmail.com>
2025-10-03 09:49:18 +0530

    feat: added new configuration

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,43 +1,7 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+source /usr/share/cachyos-fish-config/cachyos-config.fish
 
-alias ls='eza -lh --group-directories-first --icons=auto'
-alias lsa='ls -a'
-alias lt='eza --tree --level=2 --long --icons --git'
-alias lta='lt -a'
-alias ff="fzf --preview 'bat --style=numbers --color=always {}'"
-
-# Directories
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
-
-# Tools
-alias g='git'
-alias d='docker'
-alias r='rails'
-
-starship init fish | source
-
-function n
-    nvim $argv
-end
-mise activate fish | source
-# if "usage" is not installed show an error
-if ! command -v usage &> /dev/null
-    echo >&2
-    echo "Error: usage CLI not found. This is required for completions to work in mise." >&2
-    echo "See https://usage.jdx.dev for more information." >&2
-    return 1
-end
-
-if ! set -q _usage_spec_mise_2025_8_7
-  set -g _usage_spec_mise_2025_8_7 (mise usage | string collect)
-end
-set -l tokens
-if commandline -x >/dev/null 2>&1
-    complete -xc mise -a '(usage complete-word --shell fish -s "$_usage_spec_mise_2025_8_7" -- (commandline -xpc) (commandline -t))'
-else
-    complete -xc mise -a '(usage complete-word --shell fish -s "$_usage_spec_mise_2025_8_7" -- (commandline -opc) (commandline -t))'
-end
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
